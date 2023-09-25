8c61f6bb99cc9ccb5f731677cd9dd16078ca2dbb
dalinec <dalinec123@gmail.com>
2023-09-25 11:55:01 +0200

    add card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import PostCard from '@/components/PostCard/PostCard';
+
 export default function Home() {
   return (
     <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
-      <h1>Home</h1>
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
     </main>
   );
 }
