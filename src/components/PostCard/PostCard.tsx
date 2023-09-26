1266483523ac10bddaf5c9277501f6203f657371
dalinec <dalinec123@gmail.com>
2023-09-26 11:41:09 +0200

    add getPosts, getPost functionality, updated the postcard and details page

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,13 +1,22 @@
+import { Tag } from '@prisma/client';
 import Link from 'next/link';
 
-const PostCard = () => {
+interface PostCardProps {
+  title: string;
+  content: string;
+  id: string;
+  tag: Tag;
+}
+
+const PostCard = ({ title, content, id, tag }: PostCardProps) => {
   return (
     <div className='card w-full bg-base-100 shadow-xl border'>
       <div className='card-body'>
-        <h2 className='card-title'>Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className='card-title'>{title}</h2>
+        <p className='mb-4'>{content}</p>
         <div className='card-actions justify-end'>
-          <Link href={'/blog/1'} className='hover:underline'>
+          <span className='badge badge-neutral mr-auto'>{tag.name}</span>
+          <Link href={`/blog/${id}`} className='hover:underline'>
             Read More..
           </Link>
         </div>
