643c6576b55f964ef5d375a686312f1d3cd1d2ba
dalinec <dalinec123@gmail.com>
2023-09-26 12:48:32 +0200

    add delete and update routes

---
+++ src/app/api/post/[postId]/route.ts
@@ -0,0 +1,49 @@
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+interface contextProps {
+  params: {
+    postId: string;
+  };
+}
+
+export const DELETE = async (context: contextProps) => {
+  try {
+    const { params } = context;
+    await db.post.delete({
+      where: {
+        id: params.postId,
+      },
+    });
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'could not delete the post' },
+      { status: 500 }
+    );
+  }
+};
+
+export const PATCH = async (req: Request, context: contextProps) => {
+  try {
+    const { params } = context;
+    const body = await req.json();
+
+    await db.post.update({
+      where: {
+        id: params.postId,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+        tagId: body.tagId,
+      },
+    });
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'could not update the post' },
+      { status: 500 }
+    );
+  }
+};
