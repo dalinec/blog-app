7ade7d19f718dc401fbbb85e976cd49fb7e0b4b6
dalinec <dalinec123@gmail.com>
2023-09-26 13:06:25 +0200

    add delete post funcionality

--- src/components/Buttons/ButtonAction.tsx
+++ src/components/Buttons/ButtonAction.tsx
@@ -1,16 +1,50 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 import { PenSquare, Trash2 } from 'lucide-react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-const ButtonAction = () => {
+interface ButtonActionProps {
+  id: string;
+}
+
+const ButtonAction = ({ id }: ButtonActionProps) => {
+  const router = useRouter();
+
+  const { mutate: deletePost, isLoading } = useMutation({
+    mutationFn: async () => {
+      return axios.delete(`/api/post/${id}`);
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+
+    onSuccess: () => {
+      router.push('/');
+      router.refresh();
+    },
+  });
+
   return (
     <div>
       <Link href={'/edit/id'} className='btn mr-2'>
         <PenSquare />
         Edit
       </Link>
-      <button className='btn btn-error'>
-        <Trash2 />
-        Delete
+      <button onClick={() => deletePost()} className='btn btn-error'>
+        {isLoading ? (
+          <>
+            <span className='loading loading-spinner'></span>
+            <span>Loading...</span>
+          </>
+        ) : (
+          <>
+            <Trash2 />
+            Delete
+          </>
+        )}
       </button>
     </div>
   );
