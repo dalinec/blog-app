bb9a82b81d2841e33281c34dec16f5b0a2b32f45
dalinec <dalinec123@gmail.com>
2023-09-27 11:51:29 +0200

    add loading spinner between pages

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className='flex flex-col min-h-[calc(100vh-150px)] justify-center items-center'>
+      <span className='loading loading-spinner loading-lg'></span>
+    </div>
+  );
+};
+
+export default Loading;
