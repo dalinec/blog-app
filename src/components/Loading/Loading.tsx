dfe036359c4897a9982bb417286e7dc4a972dcda
dalinec <dalinec123@gmail.com>
2023-09-27 11:55:46 +0200

    update loading spinner

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className='flex flex-col min-h-[calc(100vh-150px)] justify-center items-center'>
+    <div className='flex flex-col mt-[30vh] justify-center items-center'>
       <span className='loading loading-spinner loading-lg'></span>
     </div>
   );
